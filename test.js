9e1ef7868e21b8a06db06ab33351959361bd9a7a
Tyler Band <tyler.band@ca.com>
2016-12-30 09:47:08 -0500

    local fetch

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 var apicreator = require('./APICreatorSDK');
 var api = apicreator.connect('http://localhost:8080/APIServer/rest/default/demo/v1', 'demo', 'Password1');
+console.log('connected');
 api.endpoint('demo:customer').get().then(function (data) {
 	console.log(data);
 });
